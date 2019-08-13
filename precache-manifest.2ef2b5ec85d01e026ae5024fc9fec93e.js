a9af5a86a2f62e7c087578d3018f4b9b295f2e67
hesong <actkhs@gmail.com>
2019-08-13 16:50:19 +0800

    Updates

---
+++ precache-manifest.2ef2b5ec85d01e026ae5024fc9fec93e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6211e10e469bb0badd30",
+    "url": "/reactclientpanel/static/js/main.240a70e5.chunk.js"
+  },
+  {
+    "revision": "4dddeab23a70f29b3981",
+    "url": "/reactclientpanel/static/js/runtime~main.1eb2d488.js"
+  },
+  {
+    "revision": "2e820aa198b2c62275e0",
+    "url": "/reactclientpanel/static/js/2.a2563b85.chunk.js"
+  },
+  {
+    "revision": "dfc0cda63aba4a842c70693be2325975",
+    "url": "/reactclientpanel/static/media/spinner.dfc0cda6.gif"
+  },
+  {
+    "revision": "c046c2235169bdd0b3a3e8d9c60943ac",
+    "url": "/reactclientpanel/index.html"
+  }
+];
